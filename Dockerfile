7a59d7acb9bb66d964e9b357d6fd6872c19e809e
Dan Nguyen <ndan0112@gmail.com>
2023-05-28 14:09:32 -0500

    Removed github action and update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
+#This article show how to push and pull to GAR https://cloud.google.com/artifact-registry/docs/docker/pushing-and-pulling
 FROM python:3.10-slim-buster
 
 # 
@@ -10,7 +11,7 @@ COPY ./requirements.txt /code/requirements.txt
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 
 # 
-COPY ./app /code/app
+COPY ./app /app
 
 # 
-CMD ["uvicorn", "app.main:app", "--proxy-headers", "--host", "0.0.0.0", "--port", "80"]
+CMD ["uvicorn", "main:app", "--proxy-headers", "--host", "0.0.0.0", "--port", "80"]
---
+++ main.py
@@ -0,0 +1,27 @@
+#FastAPI Stuff
+from typing import Union
+from fastapi import FastAPI
+
+
+# #Dolly Stuff
+# import torch
+# from instruct_pipeline import InstructionTextGenerationPipeline
+# from transformers import AutoModelForCausalLM, AutoTokenizer
+
+# print("Init model...")
+
+# model_name = "databricks/dolly-v2-3b"
+# tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side="left")
+# model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", torch_dtype=torch.bfloat16)
+# generate_text = InstructionTextGenerationPipeline(model=model, tokenizer=tokenizer)
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Please don't abuse this API."}
+
+# @app.post("/generate/")
+# def create_item(prompt: str):
+    
+#     return generate_text(prompt=prompt, maxlength=100)
